9c2df49fd4e5a8432b3bcbbe7c1c932be2eb3cf6
Ian Hart <ianhart7790@gmail.com>
2024-01-12 13:07:09 -0500

    start the quiz timer and stop when it hits 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
 import { quizQuestions } from "./questions.js";
 
-console.log(quizQuestions);
+// element selectors
+var startQuizBtnEl = document.getElementById('startBtn');
+var quizIntroEl = document.querySelector('.quiz-intro');
+var timerEl = document.getElementById('timer');
+
+// global variables
+var questionIndex = 0;
+var timeLeft = 60;
+
+
+function showQuestion(questionIndex) {
+}
+
+
+function hideElement(element, state) {
+    element.classList.add(state);
+}
+
+function showElement(element, state) {
+  element.classList.remove(state);
+}
+
+
+function startTimer() {
+  var intervalID = setInterval(function() {
+    timerEl.textContent = timeLeft;
+    timeLeft--;
+
+    if (timeLeft === 0) {
+      clearInterval(intervalID)
+      timeLeft = 60;
+    }
+  }, 1000)
+}
+
+function startQuiz() {
+  startTimer();
+  hideElement(quizIntroEl, 'hidden')
+  showQuestion(questionIndex);
+}
+
+// event listeners
+startQuizBtnEl.addEventListener('click', startQuiz);
